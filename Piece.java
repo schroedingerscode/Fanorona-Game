c709dc70c0bfbe9a9dc10723d76f569508f1fa56
Nicholas McHenry <RgtMcHenry@gmail.com>
2013-03-18 10:21:49 -0500

    Added the Piece class

---
+++ Piece.java
@@ -0,0 +1,9 @@
+//The dumb piece class stores all piece-related data. 
+//As of now, it only stores position and may or may not do more in the future.
+//Data only, the UI representation is handled by another class.`
+import java.awt.Point;
+
+public class Piece extends Point {
+    Piece(Point pt) { super(pt); }
+    Piece(int x, int y) { super(x, y); }
+}
