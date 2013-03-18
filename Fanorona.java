e548e97514c2631137289910b664de0480d72640
Nicholas McHenry <RgtMcHenry@gmail.com>
2013-03-18 09:42:52 -0500

    Added main() function. Also a test commit.

---
+++ Fanorona.java
@@ -0,0 +1,8 @@
+//The main game class
+//program entry point, stores # turns left, stores highest level 
+//abstractions like 'board', has main game loop
+public class Fanorona {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //TEMP
+    }
+}
