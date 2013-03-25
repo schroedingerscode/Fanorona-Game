65c6235010cc118927bccddb0d0f6658d44db84c
neb888 <neb888@tamu.edu>
2013-03-25 11:44:45 -0500

    Update AI.java

--- AI.java
+++ AI.java
@@ -3,6 +3,52 @@ import java.util.ArrayList;
 
 public class AI {
     AI() {}
+    
+    static int[][] alphaBetaSearch(int[][] gameState){
+
+        bestMove = maxValue(gameState,-99999,99999);
+
+    	return null;
+    } 
+
+
+    static int maxValue(int[][] gameState, int depth, int alpha, int beta){
+
+
+        if ( gameOver(gameState) ) return evaluateBoard(gameState);
+		
+        int bestMove = -99999;
+		ArrayList<int[][]> validMoves = getValidMoves(gameState);
+
+        for (int[][] move: validMoves){
+
+            bestMove = Math.max(bestMove,minValue(move, depth-1, alpha, beta));
+			
+            if (bestMove >= beta) return bestMove;
+			
+            alpha = Math.max(alpha, bestMove);
+        }
+        return bestMove;
+    }
+
+    static int minValue(int[][] gameState, int depth, int alpha, int beta){
+
+
+        if ( gameOver(gameState) ) return evaluateBoard(gameState);
+
+        bestMove = 99999;
+		ArrayList<int[][]> validMoves = getValidMoves(gameState);
+
+        for (int[][] move: validMoves){
+
+            bestMove = Math.min(bestMove, maxValue(move, depth-1 alpha, beta));
+			
+            if (bestMove >= beta) return bestMove;  
+			
+            beta = Math.min(beta, bestMove);
+        }
+        return bestMove;
+    }
 
     public ArrayList<Point> getMove(int[][] gridState) {
         //returns a list of points (grid coor) specifying the move
